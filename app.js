3c6b7de4f3ca0af8271de11a028acf9bbc0a7859
Federico4820 <federicolepore1@gmail.com>
2025-11-26 23:06:06 +0100

    Ottimizzazioni

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const bodyParser = require("body-parser");
-const path = require("path");
+//const path = require("path");
 const User = require("./models/user");
 const PostIt = require("./models/postIt");
-const fs = require("fs");
+//const fs = require("fs");
 let helmet = require("helmet");
 let morgan = require("morgan");
 
@@ -15,19 +17,28 @@ const sequelize = require("./utils/database");
 
 app.use(bodyParser.json());
 
+/*
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
   next();
 });
+*/
+
+app.use(cors());
 
 app.use(express.static("public"));
 app.use(helmet());
+
+/*
 const logStream = fs.createWriteStream(path.join(__dirname, "access.log"), {
   flags: "a",
 });
 app.use(morgan("combined", { stream: logStream }));
+*/
+
+app.use(morgan("dev")); // Usa "dev" o "combined"
 
 require("./routes")(app);
 
@@ -49,4 +60,8 @@ sequelize
   .catch((err) => {
     console.log("Connessione al DB error:", err);
   });
-app.listen(process.env.NODE_PORT || 8080);
+
+const port = process.env.PORT || 6969;
+app.listen(port, () => {
+  console.log(`Server avviato su porta ${port}`);
+});
